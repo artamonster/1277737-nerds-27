35abe65fa61034615c73f3ae6ecb2d08021c6cb3
Shaydulin Kirill Ramisovich <shaydulin.k.r@sblogistica.ru>
2020-04-05 05:43:44 +0300

    Комментарии к проекту

--- js/write-us.js
+++ js/write-us.js
@@ -1,7 +1,9 @@
+//var можно заменить на const.
 var link = document.querySelector(".red-contacts-button");
 var popup = document.querySelector(".feedback");
 var close = popup.querySelector(".write-us-close");
 
+//  add и remove можно попробовать заменить на toggle  в одном event
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("write-us-show");
@@ -11,6 +13,8 @@ close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("write-us-show");
 });
+
+//  можно совместить if в два условия
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
